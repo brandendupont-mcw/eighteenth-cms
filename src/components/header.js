02f1b82570441149884e03cc4ad19e14a439c57e
brandendupont-mcw <bdupont@mcw.edu>
2022-09-02 01:34:39 -0400

    go

--- src/components/header.js
+++ src/components/header.js
@@ -5,10 +5,10 @@ import React from 'react';
 const Header = ({ children, siteTitle }) => (
   <header className="usa-header usa-header--extended" role="banner">
     <div className="usa-navbar">
-      <div className="usa-logo" id="extended-logo">
-        <em className="usa-logo__text">
-          <Link to="/" title="Home" aria-label="Home">
-            {siteTitle}
+    <div className="usa-logo" id="extended-logo">
+        <em className="usa-logo__text desktop:width-tablet" >
+          <Link to="/"  title="Home" aria-label="Home">
+          Eighteenth Judicial District Attorney <br></br> Data Dashboard
           </Link>
         </em>
       </div>
